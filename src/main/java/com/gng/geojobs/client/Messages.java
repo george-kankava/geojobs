d67815cda2458791429ea3993d0c07b769ea07d2
George Kankava <george.kankava@gmail.com>
2015-03-26 22:17:15 +0400

    Initial commit

---
+++ src/main/java/com/gng/geojobs/client/Messages.java
@@ -0,0 +1,10 @@
+package com.gng.geojobs.client;
+
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  @DefaultMessage("Enter your name")
+  String nameField();
+
+  @DefaultMessage("Send")
+  String sendButton();
+}
